358c429e65e94e58a7227b8fb7f02ec1462a1248
michael.pidde@wolfnet.com <michael.pidde@wolfnet.com>
2014-10-23 12:39:03 -0500

    Fixing project. Enums.cs was not there but a references to IEnums.cs was, though the file did not actually exist in the project.

---
+++ GameTests/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTests
+{
+    class Enums
+    {
+        public enum SpriteDirection
+        {
+            North = 3,
+            South = 0,
+            East = 1,
+            West = 2
+        };
+    }
+}
