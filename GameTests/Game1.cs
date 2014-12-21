47cd618745e1baaa45133f008c8ccf7ace1fd237
Michael Pidde <michael.pidde@gmail.com>
2014-12-21 16:53:57 -0600

    Passing mapArea into Player class by reference so we don't need to do silly delegate passing.

--- GameTests/Game1.cs
+++ GameTests/Game1.cs
@@ -10,15 +10,11 @@ using Common.DataModel;
 #endregion
 
 namespace GameTests {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
+    
+
     public class Game1 : Game {
         //
-        // Constants that used to be here are now in Fn.cs. You can call me a dirty little bastard for
-        // using procedural-type static globals in an OO program, but the scope of this program
-        // allows for it. There are also some useful static functions in there. I'll determine 
-        // later if the way that I'm using that class is really a hellish implementation.
+        // Constants that used to be here are now in Fn.cs.
         //
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
@@ -35,12 +31,7 @@ namespace GameTests {
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        
         protected override void Initialize() {
             // Set window size.
             graphics.PreferredBackBufferHeight = Fn.TILESIZE * Fn.Y_TILES;
@@ -53,10 +44,7 @@ namespace GameTests {
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+
         protected override void LoadContent() {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
@@ -64,22 +52,15 @@ namespace GameTests {
             itemSheet = this.Content.Load<Texture2D>("items");
             font = this.Content.Load<SpriteFont>("font");
             Texture2D texture = this.Content.Load<Texture2D>("character_move");
-            player = new Player(texture, 1, 4, mapArea.mapData.direction, Fn.ScaleVector(mapArea.mapData.start));
+            player = new Player(texture, 1, 4, mapArea.mapData.direction, Fn.ScaleVector(mapArea.mapData.start), ref mapArea);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
+
         protected override void UnloadContent() {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+
         protected override void Update(GameTime gameTime) {
             if(GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
                 || Keyboard.GetState().IsKeyDown(Keys.Escape)) {
@@ -93,16 +74,13 @@ namespace GameTests {
                     player.ResetSprite(Fn.ScaleVector(mapArea.GetDoorPosition()), Enums.GetDirectionKey(mapArea.mapData.direction));
                     mapArea.VoidCallback();
                 }
-                player.Update(gameTime, mapArea.IsCollision, mapArea.IsDoor, mapArea.DoorAction);
+                player.Update(gameTime);
             }
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.Black);
 
--- GameTests/Player.cs
+++ GameTests/Player.cs
@@ -10,12 +10,14 @@ using Common.DataModel;
 namespace GameTests {
     class Player {
         private AnimatedSprite sprite { get; set; }
+        private MapArea mapArea { get; set; }
         private int delayedMilliseconds = 0;
 
-        public Player(Texture2D texture, int rows, int columns, int spriteDirection, Vector2 position) {
+        public Player(Texture2D texture, int rows, int columns, int spriteDirection, Vector2 position, ref MapArea mapArea) {
             int animateSpeed = 150;
             int moveTime = 300;
             this.sprite = new AnimatedSprite(texture, rows, columns, spriteDirection, position, animateSpeed, moveTime);
+            this.mapArea = mapArea;
         }
 
         public void ResetSprite(Vector2 position, Enums.SpriteDirection direction) {
@@ -27,32 +29,21 @@ namespace GameTests {
         }
 
         /*
-         * This gets called from the Update function. See commentary to that function
-         * regarding the delegates that are passed in.
+         * This gets called from the Update function.
          */
-        private void PlayerMove(Func<Vector2, bool> IsCollision, Func<Vector2, bool> IsDoor,
-            Func<Vector2, Enums.SpriteDirection, bool> DoorAction) {
+        private void PlayerMove() {
 
-            if(delayedMilliseconds > Fn.MOVE_DELAY && IsDoor(sprite.currentPosition)) {
-                DoorAction(sprite.currentPosition, sprite.spriteDirection);
+            if(delayedMilliseconds > Fn.MOVE_DELAY && mapArea.IsDoor(sprite.currentPosition)) {
+                mapArea.DoorAction(sprite.currentPosition, sprite.spriteDirection);
             }
 
-            if(delayedMilliseconds > Fn.MOVE_DELAY && !IsCollision(sprite.currentPosition)) {
+            if(delayedMilliseconds > Fn.MOVE_DELAY && !mapArea.IsCollision(sprite.currentPosition)) {
                 sprite.moving = true;
                 sprite.endPosition = Fn.ScaleVector(Fn.GetNextPosition(sprite.currentPosition));
             }
         }
 
-        /*
-         * This function is taking three functions from the MapArea as parameters. This is to avoid
-         * directly passing the instance of MapArea into this player object. Instead, we want to 
-         * use a few functions from the MapArea instance that exists elsewhere that can act on the
-         * instance, thus not needing to know exactly where the instance comes from or store a 
-         * reference to it in this object. I'm not sure whether that's a good or a bad thing
-         * in the long run, but I'll figure out and modify as I go.
-         */
-        public void Update(GameTime gameTime, Func<Vector2, bool> IsCollision, Func<Vector2, bool> IsDoor, 
-            Func<Vector2, Enums.SpriteDirection, bool> DoorAction) {
+        public void Update(GameTime gameTime) {
 
             if(sprite.moving) {
                 // Update movement time.
@@ -83,16 +74,16 @@ namespace GameTests {
 
                 if(Keyboard.GetState().IsKeyDown(Keys.Down)) {
                     sprite.Update(gameTime, Enums.SpriteDirection.South);
-                    PlayerMove(IsCollision, IsDoor, DoorAction);
+                    PlayerMove();
                 } else if(Keyboard.GetState().IsKeyDown(Keys.Right)) {
                     sprite.Update(gameTime, Enums.SpriteDirection.East);
-                    PlayerMove(IsCollision, IsDoor, DoorAction);
+                    PlayerMove();
                 } else if(Keyboard.GetState().IsKeyDown(Keys.Left)) {
                     sprite.Update(gameTime, Enums.SpriteDirection.West);
-                    PlayerMove(IsCollision, IsDoor, DoorAction);
+                    PlayerMove();
                 } else if(Keyboard.GetState().IsKeyDown(Keys.Up)) {
                     sprite.Update(gameTime, Enums.SpriteDirection.North);
-                    PlayerMove(IsCollision, IsDoor, DoorAction);
+                    PlayerMove();
                 } else {
                     // Set sprite to idle state
                     sprite.SetIdleState();
